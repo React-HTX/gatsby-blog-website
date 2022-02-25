bf24cf6405f7558383b0a79f4dabdf4c31337a8d
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-25 17:39:03 -0600

    Update welcome screen text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { StaticImage } from "gatsby-plugin-image";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>Houston React Developers Meetup.</p>
+      <p>Houston React Developers Group.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
         src="../images/clifford.jpg"
