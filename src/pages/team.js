685cc7e4d7f9135dbf03db49bc39f2e85be940b6
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-25 17:38:43 -0600

    Adding team page and to nav

---
+++ src/pages/team.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Team = () => {
+  return (
+    <Layout pageTitle="Team">
+      <h1>Team Page</h1>
+      <p>Welcome to the coolest team ever!</p>
+    </Layout>
+  );
+};
+
+export default Team;
