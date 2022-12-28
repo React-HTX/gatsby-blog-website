aef4deda8f97e4d2060e8bedf05d2f398711efc7
Fabian Buentello <fbuentello@dragos.com>
2022-12-28 16:25:42 -0600

    Add Meetup API to gatsby codebase

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,21 @@
 import React from "react";
 import Layout from "../components/layout";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
 const AboutPage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMeetupEvent(limit: 10) {
+        nodes {
+          id
+          name
+          description
+        }
+      }
+    }
+  `);
+
+  console.log(data);
   return (
     <Layout pageTitle="About Me">
       <p>
